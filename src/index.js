0c53969af815eaf5cc5cb0e2dab69b6eebcba430
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-10 09:52:46 +0530

    Basic Layout & Routing Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// React 18 (https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis)
+
+import { createRoot } from 'react-dom/client';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+        <App />
+    </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
